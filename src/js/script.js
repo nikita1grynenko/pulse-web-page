776d1ad67abbb02d4a9939b245d8bba8483b227f
Nikita Grynenko <nikitagrinenko888@gmail.com>
2024-09-04 18:05:28 +0300

    animations and scroll up added

--- src/js/script.js
+++ src/js/script.js
@@ -44,4 +44,13 @@ $(document).ready(function () {
         })
     })
 
+    $(window).scroll(function (){
+        if($(this).scrollTop() > 1600){
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    })
+
+    new WOW().init();
 });
